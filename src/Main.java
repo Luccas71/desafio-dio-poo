c1d9742b1e7be3a8c0b11e87936866d835441c3c
Lucas Volpato <lmaciel916@gmail.com>
2024-08-20 10:43:44 -0300

    Projeto concluido
    Signed-off-by: Lucas Volpato <lmaciel916@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,44 @@
+import br.com.dio.desafio.dominio.*;
+
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Curso curso1 = new Curso();
+        Curso curso2 = new Curso();
+        Mentoria mentoria = new Mentoria();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        curso1.setNome("Curso 1");
+        curso1.setDescricao("Descrição curso 1");
+        curso1.setCargaHoraria(12);
+
+        curso2.setNome("Curso 2");
+        curso2.setDescricao("Descrição curso 2");
+        curso2.setCargaHoraria(6);
+
+        mentoria.setNome("Mentoria");
+        mentoria.setDescricao("Descrição mentoria");
+        mentoria.setDate(LocalDate.now());
+
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java");
+        bootcamp.setDescricao("Descrição bootcamp Java");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devLucas = new Dev();
+        devLucas.setNome("Lucas");
+        devLucas.inscreverBootcamp(bootcamp);
+
+        System.out.println("Conteudo inscritos: " + devLucas.getConteudosInscrito());
+        devLucas.progredir();
+        devLucas.progredir();
+        System.out.println("Conteudos inscritos: " + devLucas.getConteudosInscrito());
+        System.out.println("Conteudos concluidos: " + devLucas.getConteudosConcluidos());
+        System.out.println("XP: " + devLucas.calculatTotalXp());
     }
 }
